7a439f8ae71780f6a63275440c7617fd15f2bfcf
pdxdave <webdevdave@hotmail.com>
2020-07-23 09:03:51 -0700

    added buttons and text to banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-import {Container, Grid} from 'semantic-ui-react'
+import {Container, Grid, Button} from 'semantic-ui-react'
 import styled from 'styled-components'
 
 const Home = () =>{
@@ -7,11 +7,16 @@ const Home = () =>{
         <div>
             <div className="banner">
                 <Container>
-                    <Grid divided="vertically">
+                    <Grid divided="vertically" style={{margin: "0", padding:"0"}}>
                         <Grid.Row>
                             <Grid.Column mobile={10} table={10} computer={8}>
                                 <BannerTitle>Personal Trainer</BannerTitle>
-
+                                <BannerSubTitle>Transform your life through personalized weight loss management and mind & body training</BannerSubTitle>
+                                <Button style={{marginBottom: "10px"}} color="orange">Click here</Button>
+                                <Button color="blue">Click here</Button>
+                            </Grid.Column>
+                            <Grid.Column > 
+                                 {/* empty column */}
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
@@ -39,6 +44,12 @@ const BannerTitle = styled.h1 `
     @media (min-width: 1200px) {
         padding-top: 150px;
     }
-    
+`
+const BannerSubTitle = styled.p `
+
+    @media (min-width: 768px) {
+        padding-top: 16px;
+        font-size: 2rem;
+    }
 `
 export default Home;
